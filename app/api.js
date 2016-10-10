1eb8fcf30b63e347aa24f191d691a9811d563e75
John George <joecracko@gmail.com>
2016-10-10 16:43:26 -0400

    Cleanup of API declarations

--- app/api.js
+++ app/api.js
@@ -5,4 +5,29 @@ module.exports = function(app, models){
 	    res.json(thing);
 	  })
 	});
+
+	//Example Sequelize code
+	/*app.get('/', function(req, res){
+	  connection.sync().then(function(){
+	    connection.query('\
+	      SELECT parent.* \
+	      FROM homepagertes as parent \
+	      INNER JOIN( \
+	        SELECT contentKey, MAX(createdAt) as latestCreatedAt \
+	        FROM homepagertes \
+	        GROUP BY contentKey \
+	      ) as child \
+	      ON parent.contentKey = child.contentKey AND parent.createdAt = child.latestCreatedAt \
+	    ', { model: models.HomepageRTE })
+	    .then(function(contents){
+	      var rteContent = [];
+	      contents.forEach(function(e){
+	        rteContent[e.dataValues.contentKey] = e.dataValues.content;
+	      });
+	      res.render('home', {rteContents: rteContent});
+	    });
+	  });
+
+	});
+	*/
 }
--- server.js
+++ server.js
@@ -38,31 +38,6 @@ app.use(compression())
 
 require('./app/api.js')(app, models);
 
-//Example Sequelize code
-/*app.get('/', function(req, res){
-  connection.sync().then(function(){
-    connection.query('\
-      SELECT parent.* \
-      FROM homepagertes as parent \
-      INNER JOIN( \
-        SELECT contentKey, MAX(createdAt) as latestCreatedAt \
-        FROM homepagertes \
-        GROUP BY contentKey \
-      ) as child \
-      ON parent.contentKey = child.contentKey AND parent.createdAt = child.latestCreatedAt \
-    ', { model: models.HomepageRTE })
-    .then(function(contents){
-      var rteContent = [];
-      contents.forEach(function(e){
-        rteContent[e.dataValues.contentKey] = e.dataValues.content;
-      });
-      res.render('home', {rteContents: rteContent});
-    });
-  });
-
-});
-*/
-
 if (isDeveloping) {
   const webpack = require('webpack');
   const webpackMiddleware = require('webpack-dev-middleware');
