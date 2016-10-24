097053d04066297dde655a6f6011c59f45affc6c
Joecracko <joecracko@gmail.com>
2016-10-24 09:53:59 -0400

    Adding reference to Redis in Server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var compression = require('compression')
 var bodyParser = require('body-parser')
 var passport = require('passport')
 var app = express()
-var session = require('express-session')
+var session = require('client-sessions')
 var Sequelize = require('sequelize');
 
 //Database connection and models.
@@ -19,10 +19,20 @@ var models = require('./app/ModelInitializer.js')(connection, Sequelize);
 })*/
 
 app.use(bodyParser.json())
+
+//http://www.codingscripts.com/using-database-to-handle-session-in-node-js/
 app.use(session({
-  secret: "LongSecretKey",
-  resave: false,
-  saveUninitialized: false
+  cookieName: 'ProjectNameCookie',
+  secret: 'SecretCookieString',
+  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms 
+  activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  cookie: {
+    path: '/api', // cookie will only be sent to requests under '/api' 
+    maxAge: 60000, // duration of the cookie in milliseconds, defaults to duration above 
+    ephemeral: false, // when true, cookie expires when the browser closes 
+    httpOnly: true, // when true, cookie is not accessible from javascript 
+    secure: false // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process 
+  }
 }));
 
 app.use(passport.initialize());
