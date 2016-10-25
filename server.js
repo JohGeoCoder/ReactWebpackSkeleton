2a7bc36f94ef9f55a2cca8ebd90db7cff800dc29
Joecracko <joecracko@gmail.com>
2016-10-24 22:08:35 -0400

    Switched session store to express-mysql-session

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var compression = require('compression')
 var bodyParser = require('body-parser')
 var passport = require('passport')
 var app = express()
-var session = require('client-sessions')
+var session = require('express-session')
 var Sequelize = require('sequelize');
+var MySQLStore = require('express-mysql-session')(session)
 
 //Database connection and models.
 var connection = require('./app/DatabaseInitializer.js')(Sequelize);
@@ -21,18 +22,25 @@ var models = require('./app/ModelInitializer.js')(connection, Sequelize);
 app.use(bodyParser.json())
 
 //http://www.codingscripts.com/using-database-to-handle-session-in-node-js/
+var sessionStore = new MySQLStore({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'skeleton_db',
+    checkExpirationInterval: 1 * 60 * 60 * 1000,
+    expiration: 24 * 60 * 60 * 1000,
+    createDatabaseTable: true,
+    connectionLimit: 1,
+    
+})
+
 app.use(session({
-  cookieName: 'ProjectNameCookie',
-  secret: 'SecretCookieString',
-  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms 
-  activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-  cookie: {
-    path: '/api', // cookie will only be sent to requests under '/api' 
-    maxAge: 60000, // duration of the cookie in milliseconds, defaults to duration above 
-    ephemeral: false, // when true, cookie expires when the browser closes 
-    httpOnly: true, // when true, cookie is not accessible from javascript 
-    secure: false // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process 
-  }
+    key: 'session_cookie_name',
+    secret: 'strongsecretcodehere', 
+    store: sessionStore,
+    saveUninitialized: false, // don't create session until something stored,
+    resave: false // don't save session if unmodified
 }));
 
 app.use(passport.initialize());
